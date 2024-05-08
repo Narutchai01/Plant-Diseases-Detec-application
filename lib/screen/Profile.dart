bc8351b99867679ccf9447f86d8547a1116d5ae2
loventnc <love.nattanischa2003@gmail.com>
2024-05-09 05:20:33 +0700

    profile_test

---
+++ lib/screen/Profile.dart
@@ -0,0 +1,189 @@
+import 'package:flutter/material.dart';
+
+class Profile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                    'https://i.pinimg.com/originals/d8/a2/48/d8a2482769a50e95a835e6b6653df14c.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Positioned.fill(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  SizedBox(
+                      height: MediaQuery.of(context)
+                          .padding
+                          .top), // Adjusted padding
+                  SizedBox(height: 8),
+                  Text(
+                    'Narutchai Mauensean',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'fluklnwza007@gmail.com',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(height: 24),
+                  // Wrap the buttons column with a Container for the background color
+                  Container(
+                    padding: EdgeInsets.zero, // No padding
+                    height: 460 + 48, // Adjusted height with padding
+                    width: 400, // Adjusted width for the background color
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF0F0E5),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(32),
+                        topRight: Radius.circular(32),
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Container(
+                          height: 48, // Increased to 48
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Color(0xFFF0F0E5),
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(32),
+                              topRight: Radius.circular(32),
+                            ),
+                          ),
+                          child: SizedBox.shrink(),
+                        ),
+                        SizedBox(height: 16),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Handle edit profile action
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF304D30),
+                            shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.circular(4), // Rounded corners
+                            ),
+                            padding: EdgeInsets.symmetric(
+                              vertical: 12, // Changed height to 48
+                              horizontal: 16,
+                            ),
+                          ),
+                          child: Row(
+                            children: [
+                              SizedBox(width: 12),
+                              Icon(Icons.edit, color: Colors.white),
+                              SizedBox(width: 16),
+                              Text(
+                                'Edit Profile',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 16),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Handle change password action
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF304D30),
+                            shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.circular(4), // Rounded corners
+                            ),
+                            padding: EdgeInsets.symmetric(
+                              vertical: 10,
+                              horizontal: 16,
+                            ),
+                          ),
+                          child: Row(
+                            children: [
+                              SizedBox(width: 12),
+                              Icon(Icons.lock, color: Colors.white),
+                              SizedBox(width: 16),
+                              Text(
+                                'Change Password',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 16),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Handle logout action
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF304D30),
+                            shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.circular(4), // Rounded corners
+                            ),
+                            padding: EdgeInsets.symmetric(
+                              vertical: 10,
+                              horizontal: 16,
+                            ),
+                          ),
+                          child: Row(
+                            children: [
+                              SizedBox(width: 12),
+                              Icon(Icons.exit_to_app, color: Colors.white),
+                              SizedBox(width: 16),
+                              Text(
+                                'Logout',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 16),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: Profile(),
+  ));
+}
