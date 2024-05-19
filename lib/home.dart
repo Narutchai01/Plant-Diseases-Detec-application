b19f4679d7ccac74950c00e044c9f5fe62ca22c6
Gxamsrtt023 <111553223+Gxamsrtt023@users.noreply.github.com>
2024-05-19 23:22:21 +0700

    move collections

--- lib/home.dart
+++ lib/home.dart
@@ -15,39 +15,47 @@ class _HomePageState extends State<HomePage> {
         decoration: BoxDecoration(
           image: DecorationImage(
             image: NetworkImage(
-                'https://i.pinimg.com/564x/70/d7/38/70d738462ff22bfbe2248253e3570a17.jpg'),
+                'https://i.pinimg.com/564x/6d/c8/49/6dc8498c0944216300538c726bef2dd6.jpg'),
             fit: BoxFit.cover,
             alignment: Alignment.topCenter,
           ),
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             SizedBox(height: MediaQuery.of(context).padding.top),
             SizedBox(height: 8),
-            Padding(
-              padding: const EdgeInsets.only(left: 12.0),
-              child: Text(
-                'My collections',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
+            Align(
+              alignment: Alignment.centerRight,
+              child: Padding(
+                padding: const EdgeInsets.only(right: 12.0),
+                child: Text(
+                  'My collections',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 12.0),
-              child: Text(
-                'Identification of Your Plant Disease',
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.normal,
-                  color: Colors.black,
+            Align(
+              alignment: Alignment.centerRight,
+              child: Padding(
+                padding: const EdgeInsets.only(right: 12.0),
+                child: Text(
+                  'Identification of Your Plant Disease',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
+                  ),
                 ),
               ),
             ),
-            SizedBox(height: 60),
-            Expanded(
+            SizedBox(height: 32),
+            Flexible(
+              fit: FlexFit.tight,
               child: Container(
                 padding: EdgeInsets.zero,
                 decoration: BoxDecoration(
@@ -57,53 +65,36 @@ class _HomePageState extends State<HomePage> {
                     topRight: Radius.circular(32),
                   ),
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Container(
-                      height: 32,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        color: Color(0xFFF0F0E5),
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(32),
-                          topRight: Radius.circular(32),
-                        ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      SizedBox(height: 16),
+                      PlantDiseaseItem(
+                        title: 'Cashew Bacterial blight',
+                        date: 'Mar 1 2024',
+                        imageUrl: 'https://pagacas.com/tenants/pagacas/upload/banner/benh-hai-dieu-4.jpg',
                       ),
-                      child: SizedBox.shrink(),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.zero,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          SizedBox(height: 16),
-                          Padding(
-                            padding: EdgeInsets.all(12.0),
-                          ),
-                          PlantDiseaseItem(
-                            title: 'Cashew Bacterial blight',
-                            date: 'Mar 1 2024',
-                          ),
-                          const SizedBox(height: 10),
-                          PlantDiseaseItem(
-                            title: 'Cashew Bacterial blight',
-                            date: 'Mar 1 2024',
-                          ),
-                          const SizedBox(height: 10),
-                          PlantDiseaseItem(
-                            title: 'Cashew Bacterial blight',
-                            date: 'Mar 1 2024',
-                          ),
-                          const SizedBox(height: 20),
-                          TextButton(
-                            onPressed: () {},
-                            child: const Text('See more'),
-                          ),
-                        ],
+                      const SizedBox(height: 10),
+                      PlantDiseaseItem(
+                        title: 'Cashew Bacterial blight',
+                        date: 'Mar 1 2024',
+                        imageUrl: 'https://pagacas.com/tenants/pagacas/upload/banner/benh-hai-dieu-4.jpg',
                       ),
-                    ),
-                  ],
+                      const SizedBox(height: 10),
+                      PlantDiseaseItem(
+                        title: 'Cashew Bacterial blight',
+                        date: 'Mar 1 2024',
+                        imageUrl: 'https://pagacas.com/tenants/pagacas/upload/banner/benh-hai-dieu-4.jpg',
+                      ),
+                      const SizedBox(height: 20),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text('See more'),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -117,9 +108,14 @@ class _HomePageState extends State<HomePage> {
 class PlantDiseaseItem extends StatelessWidget {
   final String title;
   final String date;
+  final String imageUrl;
 
-  const PlantDiseaseItem({Key? key, required this.title, required this.date})
-      : super(key: key);
+  const PlantDiseaseItem({
+    Key? key,
+    required this.title,
+    required this.date,
+    required this.imageUrl,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -129,17 +125,30 @@ class PlantDiseaseItem extends StatelessWidget {
         border: Border.all(color: Color.fromARGB(255, 17, 17, 17)),
         borderRadius: BorderRadius.circular(8),
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: Row(
         children: [
-          Text(
-            title,
-            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          Image.network(
+            imageUrl,
+            width: 50,
+            height: 50,
+            fit: BoxFit.cover,
           ),
-          const SizedBox(height: 8),
-          Text(
-            date,
-            style: const TextStyle(fontSize: 14, color: Colors.grey),
+          SizedBox(width: 16),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  date,
+                  style: const TextStyle(fontSize: 14, color: Colors.grey),
+                ),
+              ],
+            ),
           ),
         ],
       ),
