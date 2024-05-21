377e0b5c039b7e7b96c530939c762172e1c872cf
Narutchai <koonfluk2015@gmail.com>
2024-05-21 23:23:33 +0700

    fecth data from api

--- lib/screen/Profile.dart
+++ lib/screen/Profile.dart
@@ -1,12 +1,67 @@
+import 'package:capstonec/Models/Account.dart';
+import 'package:capstonec/Models/Model.dart';
+import 'package:capstonec/utils/SharePreferrences.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
-class Profile extends StatelessWidget {
+Account accountuser = Account(
+  id: 0,
+  name: '',
+  email: '',
+  createdAt: DateTime.now(),
+);
+
+class Profile extends StatefulWidget {
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  final dio = Dio();
+  @override
+  void initState() {
+    super.initState();
+    getAccount();
+  }
+
+  Future<GetToken> getPrefereces() async {
+    String Token = (await SharePreferrences().getToken()) ?? '';
+    int Id = (await SharePreferrences().getId()) ?? 0;
+    GetToken gettoken = GetToken(token: Token, id: Id);
+    return gettoken;
+  }
+
+  Future<void> getAccount() async {
+    GetToken gettoken = await getPrefereces();
+    await dio
+        .get('http://10.63.201.157:3000/account/${gettoken.id}',
+            options: Options(headers: {
+              'x-jwt-token': gettoken.token,
+            }))
+        .then((res) {
+      Account account = Account.fromJson(res.data);
+      accountToJson(account);
+      accountuser = account;
+      return accountuser;
+    }).catchError((err) {
+      print(err);
+    });
+  }
+
+
+  void handleLogout()  {
+     SharePreferrences().removeToken();
+     SharePreferrences().removeId();
+    Navigator.pushNamedAndRemoveUntil(context, '/login', (route) => false);
+  }
+
   @override
   Widget build(BuildContext context) {
+    // print(accountuser.email);
     return SafeArea(
       child: Scaffold(
         body: Container(
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             image: DecorationImage(
               image: NetworkImage(
                   'https://i.pinimg.com/564x/70/d7/38/70d738462ff22bfbe2248253e3570a17.jpg'),
@@ -24,12 +79,14 @@ class Profile extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
                       SizedBox(height: MediaQuery.of(context).padding.top),
-                      SizedBox(height: 8),
+                      const SizedBox(height: 8),
                       Padding(
                         padding: const EdgeInsets.only(left: 12.0),
                         child: Text(
-                          'Narutchai Mauensean',
-                          style: TextStyle(
+                          accountuser != null && accountuser.name != null
+                              ? '${accountuser.name}'
+                              : 'No name available',
+                          style: const TextStyle(
                             fontSize: 20,
                             fontWeight: FontWeight.bold,
                             color: Colors.black,
@@ -39,20 +96,22 @@ class Profile extends StatelessWidget {
                       Padding(
                         padding: const EdgeInsets.only(left: 12.0),
                         child: Text(
-                          'fluklnwza007@gmail.com',
-                          style: TextStyle(
-                            fontSize: 14,
+                          accountuser != null && accountuser.email != null
+                              ? '${accountuser.email}'
+                              : 'No email available',
+                          style: const TextStyle(
+                            fontSize: 13,
                             fontWeight: FontWeight.normal,
                             color: Colors.black,
                           ),
                         ),
                       ),
-                      SizedBox(height: 24),
+                      const SizedBox(height: 24),
                       Container(
                         padding: EdgeInsets.zero,
                         height: 600,
                         width: 400,
-                        decoration: BoxDecoration(
+                        decoration: const BoxDecoration(
                           color: Color(0xFFF0F0E5),
                           borderRadius: BorderRadius.only(
                             topLeft: Radius.circular(32),
@@ -65,38 +124,40 @@ class Profile extends StatelessWidget {
                             Container(
                               height: 32,
                               width: double.infinity,
-                              decoration: BoxDecoration(
+                              decoration: const BoxDecoration(
                                 color: Color(0xFFF0F0E5),
                                 borderRadius: BorderRadius.only(
                                   topLeft: Radius.circular(32),
                                   topRight: Radius.circular(32),
                                 ),
                               ),
-                              child: SizedBox.shrink(),
+                              child: const SizedBox.shrink(),
                             ),
                             Padding(
                               padding: EdgeInsets.zero,
                               child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.stretch,
                                 children: [
-                                  SizedBox(height: 16),
+                                  const SizedBox(height: 16),
                                   Padding(
-                                    padding: EdgeInsets.all(12.0),
+                                    padding: const EdgeInsets.all(12.0),
                                     child: ElevatedButton(
                                       onPressed: () {},
                                       style: ElevatedButton.styleFrom(
-                                        backgroundColor: Color(0xFF304D30),
+                                        backgroundColor:
+                                            const Color(0xFF304D30),
                                         shape: RoundedRectangleBorder(
                                           borderRadius:
                                               BorderRadius.circular(4),
                                         ),
-                                        padding: EdgeInsets.symmetric(
+                                        padding: const EdgeInsets.symmetric(
                                           vertical: 12,
                                           horizontal: 16,
                                         ),
-                                        minimumSize: Size(double.infinity, 42),
+                                        minimumSize:
+                                            const Size(double.infinity, 42),
                                       ),
-                                      child: Row(
+                                      child: const Row(
                                         children: [
                                           SizedBox(width: 12),
                                           Icon(Icons.edit, color: Colors.white),
@@ -114,22 +175,24 @@ class Profile extends StatelessWidget {
                                     ),
                                   ),
                                   Padding(
-                                    padding: EdgeInsets.all(12.0),
+                                    padding: const EdgeInsets.all(12.0),
                                     child: ElevatedButton(
                                       onPressed: () {},
                                       style: ElevatedButton.styleFrom(
-                                        backgroundColor: Color(0xFF304D30),
+                                        backgroundColor:
+                                            const Color(0xFF304D30),
                                         shape: RoundedRectangleBorder(
                                           borderRadius:
                                               BorderRadius.circular(4),
                                         ),
-                                        padding: EdgeInsets.symmetric(
+                                        padding: const EdgeInsets.symmetric(
                                           vertical: 12,
                                           horizontal: 16,
                                         ),
-                                        minimumSize: Size(double.infinity, 42),
+                                        minimumSize:
+                                            const Size(double.infinity, 42),
                                       ),
-                                      child: Row(
+                                      child: const Row(
                                         children: [
                                           SizedBox(width: 12),
                                           Icon(Icons.change_circle,
@@ -148,22 +211,26 @@ class Profile extends StatelessWidget {
                                     ),
                                   ),
                                   Padding(
-                                    padding: EdgeInsets.all(12.0),
+                                    padding: const EdgeInsets.all(12.0),
                                     child: ElevatedButton(
-                                      onPressed: () {},
+                                      onPressed: () {
+                                        handleLogout();
+                                      },
                                       style: ElevatedButton.styleFrom(
-                                        backgroundColor: Color(0xFF304D30),
+                                        backgroundColor:
+                                            const Color(0xFF304D30),
                                         shape: RoundedRectangleBorder(
                                           borderRadius:
                                               BorderRadius.circular(4),
                                         ),
-                                        padding: EdgeInsets.symmetric(
+                                        padding: const EdgeInsets.symmetric(
                                           vertical: 12,
                                           horizontal: 16,
                                         ),
-                                        minimumSize: Size(double.infinity, 42),
+                                        minimumSize:
+                                            const Size(double.infinity, 42),
                                       ),
-                                      child: Row(
+                                      child: const Row(
                                         children: [
                                           SizedBox(width: 12),
                                           Icon(Icons.exit_to_app,
@@ -197,10 +264,4 @@ class Profile extends StatelessWidget {
       ),
     );
   }
-}
-
-void main() {
-  runApp(MaterialApp(
-    home: Profile(),
-  ));
 }
