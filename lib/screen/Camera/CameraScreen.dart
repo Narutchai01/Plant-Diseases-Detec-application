3fb7b3321093cf59564f9dd2899073de4406e1ae
Narutchai <koonfluk2015@gmail.com>
2024-05-23 15:52:26 +0700

    update

--- lib/screen/Camera/CameraScreen.dart
+++ lib/screen/Camera/CameraScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:image_picker/image_picker.dart';
 
 
 
@@ -22,7 +23,7 @@ class _CameraScreenState extends State<CameraScreen> {
 
   void startCamera() async {
     cameras = await availableCameras();
-    controller = CameraController(cameras[0], ResolutionPreset.medium);
+    controller = CameraController(cameras[1], ResolutionPreset.high);
     await controller.initialize().then((value) {
       if (!mounted) {
         return;
@@ -30,6 +31,8 @@ class _CameraScreenState extends State<CameraScreen> {
       setState(() {});
     }).catchError((e) => print(e));
   }
+
+
 
   @override
   void dispose() {
@@ -39,25 +42,58 @@ class _CameraScreenState extends State<CameraScreen> {
 
   @override
   Widget build(BuildContext context) {
-   if (controller == null || !controller.value.isInitialized) {
+    if (controller.value.isInitialized){
+      return Scaffold(
+        body: Stack(
+          children: [
+
+            CameraPreview(controller),
+           Align(
+              alignment: Alignment.topLeft,
+                child: IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  onPressed: () async {
+                    try {
+                      final image = await controller.takePicture();
+                      print(image.path);
+                    } catch (e) {
+                      print(e);
+                    }
+                  },
+                ),
+           ),
+           Align(
+             alignment: Alignment.bottomLeft,
+              child: IconButton(
+                icon: const Icon(Icons.image_outlined),
+                onPressed: () async {
+                },
+              ),
+           ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: IconButton(
+                icon: const Icon(Icons.circle_outlined),
+                onPressed: () async {
+                  try {
+                    final image = await controller.takePicture();
+                    print(image.path);
+                  } catch (e) {
+                    print(e);
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      );
+    } else {
       return Container();
     }
-    return Scaffold(
-      body: Stack(
-        children: [
-          CameraPreview(controller),
-          Positioned(
-            bottom: 20,
-            left: 20,
-            child: FloatingActionButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Icon(Icons.arrow_back),
-            ),
-          ),
-        ],
-      ),
-    );
   }
 }
+
+// Future _pickerImage() async {
+//   final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+//   setState()
+// }
---
+++ lib/screen/Image Picker/ImagePicker.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+
+class ImagePicker extends StatefulWidget {
+  const ImagePicker({super.key});
+
+  @override
+  _ImagePickerState createState() => _ImagePickerState();
+}
+
+
+class _ImagePickerState extends State<ImagePicker> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
